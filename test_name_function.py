0057be1167abb5f3e13543324603ee2ceec81efd
zhoushan <18230343435@163.com>
2019-01-08 10:25:16 +0800

    second_commit

--- test_name_function.py
+++ test_name_function.py
@@ -5,3 +5,5 @@ class NamesTestCase(unittest.TestCase):
     def test_first_last_name(self):
         formatted_name = get_formatted_name("zhou","shan")
         self.assertEqual(formatted_name,"Zhou Shan")
+if __name__ == "__main__":
+    unittest.main()
